ed4d37c8b2a9296587ddaa9c57c8e0a23fca7969
volant97 <volant97@naver.com>
2023-11-29 19:32:20 +0900

    [REFACT] RTK

--- src/redux/modules/letters.js
+++ src/redux/modules/letters.js
@@ -1,37 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
 import fakeData from "fakeData.json";
 
+// 순수 리덕스
 // Action Value
-// 팬레터 추가
-const ADD_LETTER = "letters/ADD_LETTER";
-// 팬레터 삭제
-const DELETE_LETTER = "letters/DELETE_LETTER";
-// 팬레터 수정
-const EDIT_LETTER = "letters/EDIT_LETTER";
+// // 팬레터 추가
+// const ADD_LETTER = "letters/ADD_LETTER";
+// // 팬레터 삭제
+// const DELETE_LETTER = "letters/DELETE_LETTER";
+// // 팬레터 수정
+// const EDIT_LETTER = "letters/EDIT_LETTER";
 
 // Action Creator
-export const addLetter = (payload) => {
-  return { type: ADD_LETTER, payload };
-};
-export const deleteLetter = (payload) => {
-  return { type: DELETE_LETTER, payload };
-};
-export const editLetter = (payload) => {
-  return { type: EDIT_LETTER, payload };
-};
+// export const addLetter = (payload) => {
+//   return { type: ADD_LETTER, payload };
+// };
+// export const deleteLetter = (payload) => {
+//   return { type: DELETE_LETTER, payload };
+// };
+// export const editLetter = (payload) => {
+//   return { type: EDIT_LETTER, payload };
+// };
 
 // initialState
-const initialState = fakeData;
+// const initialState = fakeData;
 
 // Reducer
-const letters = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_LETTER:
+// const letters = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_LETTER:
+//       const newLetter = action.payload;
+//       return [newLetter, ...state];
+//     case DELETE_LETTER:
+//       const letterId = action.payload;
+//       return state.filter((letter) => letter.id !== letterId);
+//     case EDIT_LETTER:
+//       const { id, editingText } = action.payload;
+//       return state.map((letter) => {
+//         if (letter.id === id) {
+//           return { ...letter, content: editingText };
+//         }
+//         return letter;
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+// RTK
+// Action Value 필요 X, initialState만 있으면 OK, Action Creator + Reducer 한 번에 해결
+const initialState = fakeData;
+
+const lettersSlice = createSlice({
+  name: "letters",
+  initialState,
+  reducers: {
+    addLetter: (state, action) => {
       const newLetter = action.payload;
       return [newLetter, ...state];
-    case DELETE_LETTER:
+    },
+    deleteLetter: (state, action) => {
       const letterId = action.payload;
       return state.filter((letter) => letter.id !== letterId);
-    case EDIT_LETTER:
+    },
+    editLetter: (state, action) => {
       const { id, editingText } = action.payload;
       return state.map((letter) => {
         if (letter.id === id) {
@@ -39,9 +70,9 @@ const letters = (state = initialState, action) => {
         }
         return letter;
       });
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export default letters;
+export default lettersSlice.reducer;
+export const { addLetter, deleteLetter, editLetter } = lettersSlice.actions;
