7e344da37ab2f035ce28666520f545a2e58084df
volant97 <volant97@naver.com>
2023-11-29 20:03:23 +0900

    [REFACT] RTK

---
+++ src/redux/modules/authSlice.js
@@ -0,0 +1,3 @@
+// const initialState = false
+
+// const
--- src/redux/modules/member.js
+++ src/redux/modules/member.js
@@ -1,19 +1,41 @@
-const SET_MEMBER = "member/SET_MEMBER";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const setMember = (payload) => {
-  return { type: SET_MEMBER, payload };
-};
+// 순수 리덕스
+// Action Value
+// const SET_MEMBER = "member/SET_MEMBER";
 
+// Action Creator
+// export const setMember = (payload) => {
+//   return { type: SET_MEMBER, payload };
+// };
+
+// initialState
+// const initialState = "카리나";
+
+// Reducer
+// const member = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_MEMBER:
+//       const activeMember = action.payload;
+//       return activeMember;
+//     default:
+//       return state;
+//   }
+// };
+
+// RTK
 const initialState = "카리나";
 
-const member = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_MEMBER:
+const memberSlice = createSlice({
+  name: "member",
+  initialState,
+  reducers: {
+    setMember: (state, action) => {
       const activeMember = action.payload;
       return activeMember;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export default member;
+export default memberSlice.reducer;
+export const { setMember } = memberSlice.actions;
